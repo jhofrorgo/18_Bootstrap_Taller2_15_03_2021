f1f374346622255098df93cc64170ddc46a4a4c8
jhofrorgo <jhofrgo@gmail.com>
2021-03-22 11:41:19 -0500

    ok

---
+++ Bootstrap_Taller2_15_03_2021-master/Profesor/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+
+
+    $(".btn.btn-primary").attr({'href': 'https://pruebacofenalco.000webhostapp.com/Bootstrap_Taller1_12_03_2021/Profesor/', "target" : "_blank"});
+
+
+
+
+
+
+
+});
